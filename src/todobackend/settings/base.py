8cd83478a3285662b3543700e04dab9dfa3fcaf0
krishna.sharma1408 <krishna.sharma1408@gmail.com>
2018-01-13 09:22:34 +0000

    updates

--- src/todobackend/settings/base.py
+++ src/todobackend/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7b=3cc6zf4)_-u(oq49j=&=2_lc0j0s(#2$r*g2$)tw$gan2!^'
+SECRET_KEY = '#%8=y*l-+z*+mot(0+17@lm%!_yt4zb*j69fag-x((vz^zif(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,5 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Settings
 
 CORS_ORIGIN_ALLOW_ALL = True
