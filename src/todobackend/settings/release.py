4d8a40f8268265511888987bc61e453a41f98219
root <root@ip-192-168-4-48.eu-central-1.compute.internal>
2018-01-15 10:18:19 +0000

    info

--- src/todobackend/settings/release.py
+++ src/todobackend/settings/release.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
         'USER': os.environ.get('MYSQL_USER','todo'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','Mysql@1234'),
         'HOST': os.environ.get('MYSQL_HOST','localhost'),
         'PORT': os.environ.get('MYSQL_PORT','3306'),
     }
